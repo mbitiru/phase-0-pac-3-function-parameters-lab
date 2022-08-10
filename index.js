057c8408690429a799bc341079e6c413de06156f
tanveer.chege@student.moringaschool.com <tanveer.chege@student.moringaschool.com>
2022-08-10 16:33:02 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function introduction (name) {
+  return `Hi, my name is ${name}.`
+}
+
+let name="Tanveer";
+
+function introductionWithLanguage (name,language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  }
+
+  let language="index.js";
+
+function introductionWithLanguageOptional (name,language ="JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+  }
